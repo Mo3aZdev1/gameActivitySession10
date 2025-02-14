04b91876eb38ff8edf84abe8107d39907553644c
OmarTawfik <xxcringleyxx@gmail.com>
2025-02-14 20:25:48 +0200

    Displaying results added

--- src/check_game_over/check_game_over.js
+++ src/check_game_over/check_game_over.js
@@ -17,11 +17,11 @@ export function checkGameOver() {
             setTimeout(resolve, 2000)
         }
         else {
-            setTimeout(reject, 2000, "Game over!")
+            setTimeout(reject, 2000)
         }
     });
   }
 
 checkGameOver()
     .then(() => console.log("Continue playing"))
-    .catch((error) => console.log(error));
+    .catch(() => console.log(`Game over!\n---------\nScore: 20\nDamage: 48`));
